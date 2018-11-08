950f0e6ee8bf6a760be27ff207cc4c0468fc8c8a
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-11-08 11:55:35 +0100

    add POST /todos

--- server/server.js
+++ server/server.js
@@ -1,45 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
-const todo1 = new Todo({
-  text: 'Finish Node course'
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  })
 });
 
-const todo2 = new Todo({
-  text: 'Do sport',
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
 
-const todo3 = new Todo({
-  text: 'Drink tea',
-  completed: true,
-  completedAt: 12345
-});
+// const todo1 = new Todo({
+//   text: 'Finish Node course'
+// });
 
-todo1.save().then((doc) => {
-  console.log('Saved todo', todo1)
-}, (e) => {
-  console.log('Unable to save todo', e)
-});
+// const todo2 = new Todo({
+//   text: 'Do sport',
+// });
 
-todo2.save();
-todo3.save();
+// const todo3 = new Todo({
+//   text: 'Drink tea',
+//   completed: true,
+//   completedAt: 12345
+// });
 
-const user1 = new User({
-  name: 'Lio',
-  age: 28,
-  email: 'lio@yopmail.com'
-});
+// todo1.save().then((doc) => {
+//   console.log('Saved todo', todo1)
+// }, (e) => {
+//   console.log('Unable to save todo', e)
+// });
 
-const user2 = new User({
-  name: 'Val',
-  email: 'val@yopmail.com'
-});
+// todo2.save();
+// todo3.save();
 
-user1.save().then((doc) => {
-  console.log('Saved user', user1);
-}, (e) => {
-  console.log('Unable to save user', e)
-});
+// const user1 = new User({
+//   name: 'Lio',
+//   age: 28,
+//   email: 'lio@yopmail.com'
+// });
 
-user2.save();
+// const user2 = new User({
+//   name: 'Val',
+//   email: 'val@yopmail.com'
+// });
+
+// user1.save().then((doc) => {
+//   console.log('Saved user', user1);
+// }, (e) => {
+//   console.log('Unable to save user', e)
+// });
+
+// user2.save();

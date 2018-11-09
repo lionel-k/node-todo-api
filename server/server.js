7e3d608c34efc054136d637421c38e22e749e951
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2018-11-09 10:21:57 +0100

    add route GET todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -29,50 +30,25 @@ app.get('/todos', (req, res) => {
   })
 });
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send({})
+    }
+    res.send({todo})
+  }).catch((e) => {
+    return res.status(404).send()
+  });
+
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
 
 module.exports = {app};
-
-// const todo1 = new Todo({
-//   text: 'Finish Node course'
-// });
-
-// const todo2 = new Todo({
-//   text: 'Do sport',
-// });
-
-// const todo3 = new Todo({
-//   text: 'Drink tea',
-//   completed: true,
-//   completedAt: 12345
-// });
-
-// todo1.save().then((doc) => {
-//   console.log('Saved todo', todo1)
-// }, (e) => {
-//   console.log('Unable to save todo', e)
-// });
-
-// todo2.save();
-// todo3.save();
-
-// const user1 = new User({
-//   name: 'Lio',
-//   age: 28,
-//   email: 'lio@yopmail.com'
-// });
-
-// const user2 = new User({
-//   name: 'Val',
-//   email: 'val@yopmail.com'
-// });
-
-// user1.save().then((doc) => {
-//   console.log('Saved user', user1);
-// }, (e) => {
-//   console.log('Unable to save user', e)
-// });
-
-// user2.save();
